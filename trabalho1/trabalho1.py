b551ccddce46486a85a6aa7629f33944b92492ff
Bruno Berton <bberton@ciandt.com>
2018-03-20 22:47:52 -0300

    add contours filter and some metrics

--- trabalho1/trabalho1.py
+++ trabalho1/trabalho1.py
@@ -5,11 +5,7 @@ import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import misc
-from skimage import exposure
-
-
-def rgb_to_gray(rgb):
-    return np.dot(rgb, [0.299, 0.587, 0.114])
+from skimage import measure
 
 
 def main(arguments):
@@ -21,15 +17,50 @@ def main(arguments):
         print("File {} not found.".format(args.image_path))
         exit(0)
 
-    # print(image.shape)
-    # print(image.shape[0])
-    # print(image.shape[1])
-    # print(image.shape[2])
-    # print(image[0].shape)
-    # print(image[40][200])
-
-    gray_image = rgb_to_gray(image)
+    gray_image = extract_gray_image(image)
     misc.imsave('gray-image.png', gray_image)
+
+    contours = extract_contours(gray_image)
+
+    contours_image = save_contours_image(contours, gray_image.shape)
+    print(contours_image)
+    labels, num_labels = measure.label(gray_image, return_num=True)
+    print(labels)
+    print(num_labels)
+    props = measure.regionprops(labels)
+
+    for prop in props:
+        print("região: {} perímetro: {} área: {}".format(prop.perimeter, prop.label, prop.area))
+
+
+def extract_gray_image(color_image):
+    return np.round(rgb_to_gray(color_image))
+
+
+def rgb_to_gray(rgb):
+    return np.dot(rgb, [0.299, 0.587, 0.114])
+
+
+def extract_contours(gray_image):
+    return measure.find_contours(gray_image, 0.8)
+
+
+def save_contours_image(contours, shape):
+    empty_image = np.full(shape, 255, dtype=np.uint8)
+
+    # Display the image and plot all contours found
+    fig, ax = plt.subplots()
+    plt.imshow(empty_image, interpolation='nearest', cmap='gray', shape=shape, vmin=0, vmax=255)
+
+    for contour in contours:
+        plt.plot(contour[:, 1], contour[:, 0], linewidth=2, color='black')
+
+    ax.axis('image')
+    ax.set_xticks([])
+    ax.set_yticks([])
+    plt.savefig('contours.png', bbox_inches='tight', pad_inches=0, cmap='gray')
+
+    return misc.imread('contours.png')
 
 
 if __name__ == "__main__":
