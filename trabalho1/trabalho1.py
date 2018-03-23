af5345a98c3bf983a40b0fc6cfd7ce453fec0819
Bruno Berton <bberton@ciandt.com>
2018-03-22 22:21:47 -0300

    fix statistics

--- trabalho1/trabalho1.py
+++ trabalho1/trabalho1.py
@@ -22,19 +22,20 @@ def main(arguments):
 
     contours = extract_contours(gray_image)
 
-    contours_image = save_contours_image(contours, gray_image.shape)
-    print(contours_image)
-    labels, num_labels = measure.label(gray_image, return_num=True)
-    print(labels)
-    print(num_labels)
-    props = measure.regionprops(labels)
+    save_contours_image(contours, gray_image.shape)
 
-    for prop in props:
-        print("região: {} perímetro: {} área: {}".format(prop.perimeter, prop.label, prop.area))
+    # anything that is not background(white - 255) is gonna change to black
+    gray_image[gray_image < 255] = 0
+
+    labels, num_labels = measure.label(gray_image, return_num=True, background=255)
+    props = measure.regionprops(labels, intensity_image=gray_image)
+
+    print_statistics(props)
+    save_labeled_image(image, props)
 
 
 def extract_gray_image(color_image):
-    return np.round(rgb_to_gray(color_image))
+    return np.round(rgb_to_gray(color_image)).astype(int)
 
 
 def rgb_to_gray(rgb):
@@ -50,17 +51,56 @@ def save_contours_image(contours, shape):
 
     # Display the image and plot all contours found
     fig, ax = plt.subplots()
-    plt.imshow(empty_image, interpolation='nearest', cmap='gray', shape=shape, vmin=0, vmax=255)
+    ax.imshow(empty_image, interpolation='nearest', cmap='gray', shape=shape, vmin=0, vmax=255)
 
     for contour in contours:
-        plt.plot(contour[:, 1], contour[:, 0], linewidth=2, color='black')
+        ax.plot(contour[:, 1], contour[:, 0], linewidth=2, color='black')
 
     ax.axis('image')
     ax.set_xticks([])
     ax.set_yticks([])
-    plt.savefig('contours.png', bbox_inches='tight', pad_inches=0, cmap='gray')
+    fig.savefig('contours.png', bbox_inches='tight', pad_inches=0, cmap='gray')
 
     return misc.imread('contours.png')
+
+
+def print_statistics(props):
+    num_small_regions = 0
+    num_medium_regions = 0
+    num_big_regions = 0
+
+    print("número de regiões: {}".format(len(props)))
+    print()
+    for prop in props:
+        print("região: {} perímetro: {:.2f} área: {}".format(prop.label - 1, prop.perimeter, prop.area))
+        if prop.area < 1500:
+            num_small_regions += 1
+
+        if 1500 <= prop.area < 3000:
+            num_medium_regions += 1
+
+        if prop.area >= 3000:
+            num_big_regions += 1
+
+    print()
+    print("número de regiões pequenas: {}".format(num_small_regions))
+    print("número de regiões médias: {}".format(num_medium_regions))
+    print("número de regiões grandes: {}".format(num_big_regions))
+
+
+def save_labeled_image(image, props):
+    # Display the image and plot all contours found
+    fig, ax = plt.subplots()
+    ax.imshow(image)
+
+    for prop in props:
+        ax.text(prop.centroid[1] - 5, prop.centroid[0] + 5, str(prop.label - 1))
+
+    ax.axis('image')
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    plt.savefig('labeled-image.png', bbox_inches='tight', pad_inches=0)
 
 
 if __name__ == "__main__":
