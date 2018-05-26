39a9c1e4bf2e8e9235f2534ba20221bdf18cb143
Bruno Berton <bberton@ciandt.com>
2018-05-26 14:16:35 -0300

    adding bicubic and lagrange method

--- trabalho4/src/trabalho4.py
+++ trabalho4/src/trabalho4.py
@@ -18,6 +18,12 @@ def main(arguments):
     resized2 = bilinear(image, 0.5)
     misc.imsave("resized_bilinear.png", resized2)
 
+    resized3 = bicubic(image, 0.5)
+    misc.imsave("resized_bicubic.png", resized3)
+
+    resized4 = lagrange(image, 0.5)
+    misc.imsave("resized_lagrange.png", resized4)
+
 
 def nearest_neighbor(image, scale):
     print("Using nearest neighbor method...")
@@ -65,6 +71,93 @@ def bilinear(image, scale):
 
     print("Image resized!")
     return resized_image
+
+
+def bicubic(image, scale):
+    print("Using bicubic method...")
+
+    new_dimension = int(image.shape[0] * scale)
+    resized_image = np.full((new_dimension, new_dimension), 0, dtype=np.uint8)
+    number_rows, number_cols = resized_image.shape
+
+    # for each row
+    for row in range(number_rows):
+        # for each column
+        for column in range(number_cols):
+            x = int(row / scale)
+            y = int(column / scale)
+            dx = (row / scale) - x
+            dy = (column / scale) - y
+
+            new_value = 0
+            for m in range(-1, 3):
+                for n in range(-1, 3):
+                    print(x + m)
+                    normalized_x = min(x + m, image.shape[0] - 1)
+                    normalized_y = min(y + n, image.shape[1] - 1)
+                    new_value += image[normalized_x][normalized_y] * r_function(m - dx) * r_function(dy - n)
+
+            resized_image[row][column] = new_value
+
+    print("Image resized!")
+    return resized_image
+
+
+def r_function(s):
+    v1 = p_function(s + 2) ** 3
+    v2 = p_function(s + 1) ** 3
+    v3 = p_function(s) ** 3
+    v4 = p_function(s - 1) ** 3
+
+    return (1 / 6) * (v1 - 4 * v2 + 6 * v3 - 4 * v4)
+
+
+def p_function(t):
+    if t > 0:
+        return t
+    else:
+        return 0
+
+
+def lagrange(image, scale):
+    print("Using Lagrange method...")
+
+    new_dimension = int(image.shape[0] * scale)
+    resized_image = np.full((new_dimension, new_dimension), 0, dtype=np.uint8)
+    number_rows, number_cols = resized_image.shape
+
+    # for each row
+    for row in range(number_rows):
+        # for each column
+        for column in range(number_cols):
+            x = int(row / scale)
+            y = int(column / scale)
+            dx = (row / scale) - x
+            dy = (column / scale) - y
+
+            resized_image[row][column] = calculate_lagrange(x, y, dx, dy, image)
+
+    print("Image resized!")
+    return resized_image
+
+
+def calculate_lagrange(x, y, dx, dy, image):
+    v1 = (-dy) * (dy - 1) * (dy - 2) * l_function(1, x, y, dx, image)
+    v2 = (dy + 1) * (dy - 1) * (dy - 2) * l_function(2, x, y, dx, image)
+    v3 = (-dy) * (dy + 1) * (dy - 2) * l_function(3, x, y, dx, image)
+    v4 = dy * (dy + 1) * (dy - 1) * l_function(4, x, y, dx, image)
+
+    return (v1 / 6) + (v2 / 2) + (v3 / 2) + (v4 / 6)
+
+
+def l_function(n, x, y, dx, image):
+    normalized_y = min(y + n - 2, image.shape[1] - 1)
+    v1 = (-dx) * (dx - 1) * (dx - 2) * image[x - 1, normalized_y]
+    v2 = (dx + 1) * (dx - 1) * (dx - 2) * image[x, normalized_y]
+    v3 = (-dx) * (dx + 1) * (dx - 2) * image[min(x + 1, image.shape[0] - 1), normalized_y]
+    v4 = dx * (dx + 1) * (dx - 1) * image[min(x + 2, image.shape[0] - 1), normalized_y]
+
+    return (v1 / 6) + (v2 / 2) + (v3 / 2) + (v4 / 6)
 
 
 def read_image(path, gray_scale=False):
